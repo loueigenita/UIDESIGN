48c57e3357a72072d014f440ecc0eba13fdf6d9e
louei <djloueigenita@gmail.com>
2023-02-17 08:09:34 +0800

    first commit

---
+++ app/src/main/java/com/example/uidesign/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.uidesign;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView txtSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        txtSignUp = findViewById(R.id.txtSignUp);
+
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
